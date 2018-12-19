0e253901082739b6b36197726dc3d59128ac5db9
AndreiBreiva <andrei.breiva@sqil.by>
2018-12-19 23:06:11 +0300

    AHW_1
    fixes: added new main layout, added show of snackbar

--- app/src/main/java/com/example/user/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/user/myapplication/MainActivity.kt
@@ -1,47 +1,35 @@
 package com.example.user.myapplication
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.TextView
-import android.widget.Toast
 
-class MainActivity : Activity() {
 
-    lateinit var textView: TextView
-    lateinit var btn: View
+class MainActivity : AppCompatActivity() {
 
-    var gameState: String? = null
-    val GAME_STATE_KEY = "asdasda"
-    val TEXT_VIEW_KEY = "asdasda2"
-
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var snackbar: Snackbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
 
-        gameState = savedInstanceState?.getString(GAME_STATE_KEY)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout) as CoordinatorLayout
+    }
 
-        setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.text_console)
-        btn = findViewById(R.id.button1)
-        btn.setOnClickListener { view ->
-          Toast.makeText(this, "пиздец!",Toast.LENGTH_LONG)
-              .show()
-        }
+    fun showMessage(v: View) {
+        Snackbar.make(findViewById(R.id.coordinatorLayout), "puff puff!", Snackbar.LENGTH_LONG)
+            .show()
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        textView.text = savedInstanceState?.getString(TEXT_VIEW_KEY)
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
-        outState?.run {
-            putString(GAME_STATE_KEY, gameState)
-            putString(TEXT_VIEW_KEY, textView.text.toString())
-        }
         super.onSaveInstanceState(outState)
     }
-
-
 }
