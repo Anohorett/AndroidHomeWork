53cb2fed3a306bc61f7a8809d1707805f272835c
andrei.breiva@gmail.com <bi9daiWaem>
2020-10-18 16:07:50 +0300

    AHW-2: Navigation: formatt onCreateView params

--- app/src/main/java/com/example/user/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/user/myapplication/FirstFragment.kt
@@ -34,7 +34,8 @@ class FirstFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
