3b2673d2ccaed62b7cc1a04b9c36408d797bb912
andrei.breiva@gmail.com <bi9daiWaem>
2020-10-20 13:55:34 +0300

    AHW-2: Navigation: added setup navigation bottom and drawer menu

--- app/src/main/java/com/example/user/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/user/myapplication/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.user.myapplication
 
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 
@@ -21,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_activity)
 
-        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val host: NavHostFragment = supportFragmentManager
@@ -30,9 +34,43 @@ class MainActivity : AppCompatActivity() {
         val navController = host.navController
 
         appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
+
+        setupBottomNavMenu(navController)
+        setupNavigationMenu(navController)
+        setupActionBar(navController, appBarConfiguration)
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+    private fun setupBottomNavMenu(navController: NavController) {
+        // Use NavigationUI to set up Bottom Navigation Menu
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
     }
 
+    private fun setupNavigationMenu(navController: NavController) {
+        // Use NavigationUI to set up a Navigation menu
+        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
+        sideNavView?.setupWithNavController(navController)
+
+    }
+
+    private fun setupActionBar(navController: NavController, appBarConfig: AppBarConfiguration) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val returnValue =  super.onCreateOptionsMenu(menu)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        if (navigationView == null) {
+            menuInflater.inflate(R.menu.overflow_menu, menu)
+            return true
+        }
+        return returnValue
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
+                || super.onOptionsItemSelected(item)
+    }
 }
