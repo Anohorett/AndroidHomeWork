109b56af639dd5eb389109d737360e0b226c32bd
andrei.breiva@gmail.com <bi9daiWaem>
2020-11-11 16:53:00 +0300

    AHW-3: added logs for fraglents lifecycle

--- app/src/main/java/com/example/user/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/user/myapplication/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.user.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +11,7 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import com.example.user.myapplication.databinding.FragmentHomeBinding
+import timber.log.Timber
 
 class HomeFragment : Fragment() {
 
@@ -24,11 +26,13 @@ class HomeFragment : Fragment() {
         setHasOptionsMenu(true)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        Timber.i("\tonCreateView")
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Timber.i("\tonViewCreated")
 
         val options = navOptions {
             anim {
@@ -51,6 +55,42 @@ class HomeFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        Timber.i("\tonDestroyView")
         _binding = null
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Timber.i("\tonAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.i("\tonCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("\tonStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("\tonResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("\tonPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("\tonStop")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Timber.i("\tonDetach")
     }
 }
