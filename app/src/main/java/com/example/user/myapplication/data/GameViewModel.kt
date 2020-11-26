32839a78d0a26c9b5026f7572ed722531b2ff29c
andrei.breiva@gmail.com <andrei.breiva@gmail.com>
2020-11-26 13:42:12 +0300

    AHW-3: added timer and transformations live data

--- app/src/main/java/com/example/user/myapplication/data/GameViewModel.kt
+++ app/src/main/java/com/example/user/myapplication/data/GameViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.user.myapplication.data
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
@@ -10,21 +13,45 @@ class GameViewModel : ViewModel() {
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
+
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
         get() = _score
+
     private val _eventGameFinish = MutableLiveData<Boolean>()
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
 
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    private val timer: CountDownTimer
+
     private lateinit var wordList: MutableList<String>
 
     init {
-
         _word.value = ""
         _score.value = 0
+
         resetList()
         nextWord()
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished / ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+        timer.start()
     }
 
     private fun resetList() {
@@ -75,7 +102,7 @@ class GameViewModel : ViewModel() {
 
     private fun nextWord() {
         if (wordList.isEmpty()) {
-            onGameFinish()
+            resetList()
         } else {
             _word.value = wordList.removeAt(0)
         }
@@ -83,6 +110,13 @@ class GameViewModel : ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
+        timer.cancel()
         Timber.i("destroyed")
+    }
+
+    companion object {
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDOWN_TIME = 60000L
     }
 }
