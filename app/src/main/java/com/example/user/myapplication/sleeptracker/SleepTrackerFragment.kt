c67e533ae7a3a9414577f876426d0fa02a55f27c
andrei.breiva@gmail.com <andrei.breiva@gmail.com>
2020-11-29 17:27:09 +0300

    AHW-3: added viewModel tosleeptracker

--- app/src/main/java/com/example/user/myapplication/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/user/myapplication/sleeptracker/SleepTrackerFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.user.myapplication.R
+import com.example.user.myapplication.database.SleepDatabase
 import com.example.user.myapplication.databinding.SleepTrackerFragmentBinding
 
 class SleepTrackerFragment : Fragment() {
@@ -18,6 +20,17 @@ class SleepTrackerFragment : Fragment() {
 
         val binding: SleepTrackerFragmentBinding =
             DataBindingUtil.inflate(inflater, R.layout.sleep_tracker_fragment, container, false)
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val sleepTrackerViewModel = ViewModelProvider(
+            this, viewModelFactory
+        ).get(SleepTrackerViewModel::class.java)
+
+        binding.setLifecycleOwner(this)
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
 
         return binding.root
     }
